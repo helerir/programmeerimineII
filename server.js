d7e5a3a3d0ad330a8cab5d3cc6b3d6d3807a1191
Heleri Roosnurm <heleri.roosnurm@gmail.com>
2017-09-14 17:11:17 +0300

    using ejs instead

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 const express = require('express'); 
 const app = express(); //käivitame express package
+const ejs = require('ejs');
 
-require('marko/node-require'); // Allow Node.js to require and load `.marko` files
-const markoExpress = require('marko/express');
+app.set('view engine', 'ejs');
 
+app.get('/', (req, res) => {
+	res.render('index', {name: 'Juku', vanus: '20'});
+});
 
-app.use(markoExpress()); //enable res.marko(template, data)
-
-const folderName = 'views';
-function importTemplate(template) {
-	return require('./' + folderName + '/' + template);
-}
-
-const indexTemplate = importTemplate('template');
-app.get('/', (req, res) => { //saadame kliendile tagasi ok. Võiks ka nt kogu <html></html> sinna kirjutada, et veebilehte kuvada, aga nii ei tehta. Selleks kasutatakse nüüd frameworki.
-	res.marko(indexTemplate, {
-		name: 'Juku'
-	});
+app.get('/pages', (req, res) => {
+	res.render('pages/index');
 });
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
-}); //saame ligi pordilt 3000
+});
